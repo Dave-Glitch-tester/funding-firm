d3c5174fef65c68d5719837bed65be7d37e73ccc
Dave-Glitch-tester <davidsoberekon66@gmail.com>
2025-02-24 03:20:48 -0800

    fixed

--- db/connect.js
+++ db/connect.js
@@ -1,12 +1,14 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
+const mongourl =
+  process.env.MONGO_URI || "mongodb://localhost:27017/funding-firm";
 
 const connectDb = () => {
-    const db = mongoose.connection
-    mongoose.connect("mongodb://localhost:27017/funding-firm")
-    db.once("open", () => {
-        console.log("connected to the Database")
-    })
-    db.on('error', console.error.bind(console, "connection error:"))
-}
+  const db = mongoose.connection;
+  mongoose.connect(mongourl);
+  db.once("open", () => {
+    console.log("connected to the Database");
+  });
+  db.on("error", console.error.bind(console, "connection error:"));
+};
 
-module.exports = connectDb
+module.exports = connectDb;
